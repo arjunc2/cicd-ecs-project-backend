e3cafc02aca4a6efffab8daf46d507037910eca6
Nagarjun <99671332+arjunc2@users.noreply.github.com>
2025-09-10 11:40:01 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const app = express();
 const port = 5000;
 
 app.get('/api/message', (req, res) => {
-  res.json({ message: 'Hello from the Backend Service - ECS Fargate!' });
+  res.json({ message: 'Hello from the Backend Service - AWS ECS Fargate!' });
 });
 
 app.listen(port, () => {
   console.log(`Backend running at http://localhost:${port}`);
 });
+
 
 
